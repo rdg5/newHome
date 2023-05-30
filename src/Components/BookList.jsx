12c9d9db8b2ae86864f5911dcce671cfa294a5f5
sanyi <sandor.vass@tuta.io>
2023-05-30 20:28:21 +0200

    feat(): add bookList component

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,9 +1,58 @@
-	import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
+// export const BookList = () => {
+// 	const db = firebase.firestore();
+
+//   const [books, setBooks] = useState([]);
+//   const [selectedYear, setSelectedYear] = useState('');
+
+//   useEffect(() => {
+//     // Fetch all books from Firestore
+//     db.collection('books').get().then((querySnapshot) => {
+//       const data = querySnapshot.docs.map((doc) => doc.data()).sort((a,b) => b.year - a.year);
+//       setBooks(data);
+//     });
+//   }, []);
+
+//   const years = ['2023', '2022', '2021', '2020'];
+
+//   const handleYearClick = (year) => {
+// 		setSelectedYear(year);
+//   };
+
+//   const filteredBooks = selectedYear ? books.filter((book) => book.year === selectedYear) : books;
+
+//   return (
+//     <div className=' flex flex-col items-center justify-center h-screen pr-7' >
+// 			<h1 className=' mb-4 text-4xl font-extrabold leading-none  text-gray-900 md:text-3xl lg:text-3xl dark:text-white pt-10'>Books that I have read in the last years</h1>
+//     	<div className='flex'>
+// 			<div className='flex flex-col items-stretch pt-7 space-y-3'>
+//         {years.map((year) => (
+//           <button className='btn-year'key={year} onClick={() => handleYearClick(year)}>
+//             {year}
+//           </button>
+//         ))}
+// 				<button className='btn-reset'key={'reset'} onClick={() => {setSelectedYear('')}}>Reset</button>
+// 			</div>
+// 			<div className='flex-row  overflow-y-auto pl-7 pt-7'>
+//       <ul className='max-w-md space-y-1 j
+//  text-orange-300 list-none list-inside object-scale-down'>
+//         {filteredBooks.map((book) => (
+//           <li className='text-xs' key={book.title}>
+//             {book.author} - {book.title}
+//           </li>
+//         ))}
+//       </ul>
+// 			</div>
+// 			</div>
+//     </div>
+//   );
+// 	}
+
 export const BookList = () => {
-	const db = firebase.firestore();
+  const db = firebase.firestore();
 
   const [books, setBooks] = useState([]);
   const [selectedYear, setSelectedYear] = useState('');
@@ -11,7 +60,7 @@ export const BookList = () => {
   useEffect(() => {
     // Fetch all books from Firestore
     db.collection('books').get().then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => doc.data()).sort((a,b) => b.year - a.year);
+      const data = querySnapshot.docs.map((doc) => doc.data()).sort((a, b) => b.year - a.year);
       setBooks(data);
     });
   }, []);
@@ -19,28 +68,38 @@ export const BookList = () => {
   const years = ['2023', '2022', '2021', '2020'];
 
   const handleYearClick = (year) => {
-		setSelectedYear(year);
+    setSelectedYear(year);
   };
 
   const filteredBooks = selectedYear ? books.filter((book) => book.year === selectedYear) : books;
 
   return (
-    <div>
-      <ul>
-        {years.map((year) => (
-          <li key={year} onClick={() => handleYearClick(year)}>
-            {year}
-          </li>
-        ))}
-				<li key={'nothing'} onClick={() => {setSelectedYear('')}}>Reset</li>
-      </ul>
-      <ul>
-        {filteredBooks.map((book) => (
-          <li key={book.title}>
-            {book.author} - {book.title}
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col items-center justify-center h-screen pr-7">
+      <h1 className="mb-4 text-4xl font-bold leading-none text-gray-900 md:text-3xl dark:text-white pt-10">
+        Books that I have read in the last years
+      </h1>
+      <div className="flex">
+        <div className="flex flex-col items-stretch pt-7 space-y-3">
+          {years.map((year) => (
+            <button className="btn-year" key={year} onClick={() => handleYearClick(year)}>
+              {year}
+            </button>
+          ))}
+          <button className="btn-reset" key="reset" onClick={() => setSelectedYear('')}>
+            Reset
+          </button>
+        </div>
+        <div className="flex flex-col overflow-y-auto pt-7 pl-7">
+          <ul className="max-w-md space-y-1 text-orange-300 list-none list-inside">
+            {filteredBooks.map((book) => (
+              <li className="text-xs" key={book.title}>
+                {book.author} - {book.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-	}
+};
+
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
 	
 	return(
 		<>
-		<h1>Hi, this is my internet thing</h1>
+		<h1 className = "text-3xl font-bold underline">Hi, this is my internet thing</h1>
 		<img src={pp}></img>
 		</>	
 	)
