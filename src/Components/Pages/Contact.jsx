a2391ca0fe4a08d6d934e818cecc4b0ec2fc6e93
sanyi <sandor.vass@tuta.io>
2023-06-07 17:51:42 +0200

    feat(): add new tab to anchor tags

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -6,13 +6,13 @@ export const Contact = () => {
       <div className="flex flex-col pb-7">
         <ul>
           <li className="text-teal-500">
-            github: <a href="https://github.com/rdg5" className="pb-1 underline text-orange-100 decoration-orange-500">rdg5</a>
+            github: <a href="https://github.com/rdg5" className="pb-1 underline text-orange-100 decoration-orange-500" target="_blank">rdg5</a>
           </li>
           <li className="text-teal-500">
-            linkedin: <a href="https://www.linkedin.com/in/sandorvass" className="pb-1 underline text-orange-100 decoration-orange-500">linkedin</a>
+            linkedin: <a href="https://www.linkedin.com/in/sandorvass" className="pb-1 underline text-orange-100 decoration-orange-500" target="_blank">linkedin</a>
           </li>
           <li className="text-teal-500">
-            twitter: <a href="https://twitter.com/not_rdg" className="pb-1 underline text-orange-100 decoration-orange-500">@not_rdg</a>
+            twitter: <a href="https://twitter.com/not_rdg" className="pb-1 underline text-orange-100 decoration-orange-500" target="_blank">@not_rdg</a>
           </li>
         </ul>
       </div>
