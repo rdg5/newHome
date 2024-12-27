c0710f762a18842c9c2fd622ba4be830945a567c
sanyi <sandor.vass@tuta.io>
2024-12-27 12:00:41 +0100

    feat(): add 2025

--- src/Components/Pages/Whoami.jsx
+++ src/Components/Pages/Whoami.jsx
@@ -1,13 +1,11 @@
 
-
 export const Whoami = () => {
 
 	return (
     <div className="flex flex-col items-center justify-center h-5/6">
 		  <div className="w-2/5">
 			  <p className="text-xl text-gray-900">
-			    I'm sanyi, currently based in Budapest. I teach programming in a  
-          <a className="underline decoration-orange-600"> bootcamp</a>.
+			    I'm sanyi, currently based in Budapest. I'm a software developer at <a href="https://bobcatscoding.com" className="underline decoration-orange-600">Bobcats coding</a>. <br/>
           Outside of work, I like to <a href="/books"class="underline decoration-teal-400">read </a> 
 		      and to<a class="underline decoration-orange-100"> cook</a>.
 		    </p>
