0ca6fe667dc814d44fc9a283c7913d083a8a4aa9
fox <box@foxs-MacBook-Pro-2.local>
2026-02-18 15:09:11 +0100

    chore(): add 2026 to the year list

--- src/Components/Pages/BookList.jsx
+++ src/Components/Pages/BookList.jsx
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { YearButton } from '../Buttons/YearButton';
-import { ResetButton } from '../Buttons/ResetButton';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import React, { useState, useEffect } from "react";
+import { YearButton } from "../Buttons/YearButton";
+import { ResetButton } from "../Buttons/ResetButton";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 export const BookList = () => {
   const db = firebase.firestore();
 
   const [books, setBooks] = useState([]);
-  const [selectedYear, setSelectedYear] = useState('');
+  const [selectedYear, setSelectedYear] = useState("");
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const queriedBooks = await db.collection('books').get();
-        const data = queriedBooks.docs.map((book) => book.data()).sort((a, b) => b.year - a.year);
+        const queriedBooks = await db.collection("books").get();
+        const data = queriedBooks.docs
+          .map((book) => book.data())
+          .sort((a, b) => b.year - a.year);
         setBooks(data);
       } catch (error) {
-        console.error('Error fetching books:', error);
+        console.error("Error fetching books:", error);
       }
     };
 
     fetchBooks();
   }, [db]);
 
-  const years = ['2025', '2024', '2023', '2022', '2021', '2020'];
+  const years = ["2026", "2025", "2024", "2023", "2022", "2021", "2020"];
 
   const handleYearClick = (year) => {
     setSelectedYear(year);
   };
 
-  const filteredBooks = selectedYear ? books.filter((book) => book.year === selectedYear) : books;
+  const filteredBooks = selectedYear
+    ? books.filter((book) => book.year === selectedYear)
+    : books;
 
   return (
     <div className="flex flex-col items-center justify-center h-5/6 pr-7">
@@ -40,9 +44,14 @@ export const BookList = () => {
       <div className="flex">
         <div className="flex flex-col justify-start items-stretch pt-7 space-y-3">
           {years.map((year) => (
-            <YearButton key={year} year={year} onClick={handleYearClick} selected={selectedYear === year} />
+            <YearButton
+              key={year}
+              year={year}
+              onClick={handleYearClick}
+              selected={selectedYear === year}
+            />
           ))}
-          <ResetButton onClick={() => setSelectedYear('')} />
+          <ResetButton onClick={() => setSelectedYear("")} />
         </div>
         <div className="flex flex-col overflow-y-auto pt-7 pl-7">
           <ul className="max-w-md space-y-1 text-orange-300 list-none list-inside">
